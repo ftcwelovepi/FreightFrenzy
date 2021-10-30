d4383e959f11a2628ad4adc438b4295494d0282d
samihsq <samih.qureshi@gmail.com>
2021-10-30 16:10:12 -0700

    set bucket pos code, doesnt work rn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Configs/TeleOpOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Configs/TeleOpOne.java
@@ -16,6 +16,15 @@ public class TeleOpOne extends Template {
     @Override
     public void a(boolean pressed) {
         if (pressed) {
+            robot.bucket.setPosition(0);
+
+        }
+    }
+
+    @Override
+    public void b(boolean pressed) {
+        if (pressed) {
+            robot.bucket.setPosition(1);
 
         }
     }
