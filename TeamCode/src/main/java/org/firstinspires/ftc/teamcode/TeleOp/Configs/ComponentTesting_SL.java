daa4350df5e76f8b392d15dc1f727409338c9080
Rohit <rohmam060405@gmail.com>
2022-01-29 17:20:34 -0800

    Update ComponentTesting_SL.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Configs/ComponentTesting_SL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Configs/ComponentTesting_SL.java
@@ -2,66 +2,91 @@ package org.firstinspires.ftc.teamcode.TeleOp.Configs;
 
 import android.transition.Slide;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.configuration.WriteXMLFileHandler;
+
 import org.firstinspires.ftc.teamcode.Hardware.Components.Bucket_Servo;
 import org.firstinspires.ftc.teamcode.Hardware.Components.Intake;
 import org.firstinspires.ftc.teamcode.Hardware.Components.Slides;
 
 public class ComponentTesting_SL extends Template{
 
+    int high = 600;
+    int low = 50;
+
+    double scale = 1;
+    double power = 0;
+
     boolean isRTPressed = false;
     boolean isLTPressed = false;
 
     public void init() {
         robot.initWheels();
-        Slides.initialize( robot );
-        Slides.scalePower( 0.7 );
-        Intake.initialize( robot );
+        robot.initSlides();
+        robot.initIntake();
+        robot.slides.setMode( DcMotor.RunMode.RUN_USING_ENCODER );
     }
 
     public void rt (float pressure) {
-        if (isRTPressed) Slides.setPower( pressure );
-        if (pressure != 0) {
-            isRTPressed = true;
-        } else {
-            isRTPressed = false;
-        }
+
     }
 
     public void lt (float pressure) {
-        if (isLTPressed) Slides.setPower( -pressure );
-        if (pressure != 0) {
-            isLTPressed = true;
-        } else {
-            isLTPressed = false;
-        }
+
     }
 
     @Override
     public void updateTelemetryDM() {
-        telemetryDM.put( "Encoders", String.valueOf( Slides.getEncoders() ) );
-//        telemetryDM.put( "Distance", String.valueOf( Slides.getDistance() ) );
+        telemetryDM.put( "Encoders", String.valueOf( robot.slides.getCurrentPosition() ) );
+        telemetryDM.put( "Highbound",  String.valueOf(high));
+        telemetryDM.put( "Lowbound", String.valueOf(low) );
+        telemetryDM.put( "Motor Power", String.valueOf( robot.slides.getPower() ) );
+        telemetryDM.put( "Power", String.valueOf( power ) );
+        telemetryDM.put( "Scale", String.valueOf( scale ) );
+
     }
 
     public void a(boolean pressed) {
         if (pressed)
-            Intake.setPower( 1 );
+            scale-=0.1;
     }
 
     @Override
     public void b(boolean pressed) {
         if (pressed)
-            Intake.setPower( -1 );
+            scale += 0.1;
     }
 
     @Override
     public void x(boolean pressed) {
+
+    }
+
+    @Override
+    public void du(boolean pressed) {
         if (pressed)
-            Intake.setPower( 0 );
+            high += 10;
+    }
+
+    public void dd(boolean pressed) {
+        if (pressed)
+            high -= 10;
+    }
+
+    public void dr(boolean pressed) {
+        if (pressed)
+            low += 10;
+    }
+
+    public void dl(boolean pressed) {
+        if (pressed)
+            low -= 10;
     }
 
     public void loop() {
-        Slides.update();
-        Intake.update();
+        if (robot.slides.getCurrentPosition() >= high && power>0) power = 0;
+        if (robot.slides.getCurrentPosition() <= low && power<0) power = 0;
+        robot.slides.setPower( power * scale );
     }
 
     public String getName() {
